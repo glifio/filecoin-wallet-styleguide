2f43f28699f471cf97abbe0e78acc500df6a0c9a
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-09 12:05:53 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+export { default as theme } from './theme';
+export { default as ThemeProvider } from './ThemeProvider';
+export { default as BaseStyles } from './BaseStyles';
+
+// layout
+export { default as Box } from './Box';
+export { default as Flex } from './Flex';
+
+// Typography
+export { default as Text } from './Text';
+export { default as Heading } from './Heading';
+
+// components
+export { default as Avatar } from './Avatar';
