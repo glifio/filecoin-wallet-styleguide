dbac2a7d16d18d02b72993a0322083f4baec83c4
Alex Singh <alex@alexsingh.com>
2020-02-11 14:05:58 -0300

    Clean theme (#5)
    * Removes buttonStyles
    
    * Removes buttonSize
    
    * Removes button color styles
    
    * Updates font declarations et al
    
    * Updates color scheme, theme.js updates to match
    
    * Removes excess color styles
    
    * Updating variable names to JS objects
    
    * Updated colors file, removed unused variants
    
    * Updates file's color variables
    
    * Comments out unused color variables

--- src/colors.js
+++ src/colors.js
@@ -5,57 +5,58 @@ const baseColors = {
   black: '#000',
   white: '#FFF',
   blue: '#36ADF1',
-  green: '#28C081',
-  yellow: '#FD9D28',
-  red: '#DC2C10',
-  blurple: '#4E3FCE',
-  consensysblue: '#3259D6',
+  green: '#1AD08F',
+  yellow: '',
+  red: '#F96C6C',
+  purple: '#5E26FF'
 };
 
 // palette
+// Keeping light/dark values in here for the future if we want to roll a dark theme
 const colors = {
-  blurple: {
-    base: baseColors.blurple,
-    text: readableColor(baseColors.blurple),
-    light: [null, tint(0.2, baseColors.blurple)],
-    dark: [null, shade(0.2, baseColors.blurple)],
+  purple: {
+    base: baseColors.purple,
+    text: '#E0D7FE'
+    // light: [null, tint(0.2, baseColors.purple)],
+    // dark: [null, shade(0.2, baseColors.purple)]
   },
-  blue: {
-    base: baseColors.blue,
-    text: readableColor(baseColors.blue),
-    light: [null, tint(0.9, baseColors.blue)],
-    dark: [null, shade(0.4, baseColors.blue)],
-  },
+  // Q: do we need blue?
+  // blue: {
+  // base: baseColors.blue,
+  // text: readableColor(baseColors.blue)
+  // light: [null, tint(0.9, baseColors.blue)],
+  // dark: [null, shade(0.4, baseColors.blue)]
+  // },
   green: {
     base: baseColors.green,
-    text: baseColors.white,
-    light: [null, tint(0.9, baseColors.green)],
-    dark: [null, shade(0.4, baseColors.green)],
+    text: '#08442F'
+    // light: [null, tint(0.9, baseColors.green)],
+    // dark: [null, shade(0.4, baseColors.green)]
   },
   yellow: {
     base: baseColors.yellow,
-    text: readableColor(baseColors.yellow),
-    light: [null, tint(0.9, baseColors.yellow)],
-    dark: [null, shade(0.4, baseColors.yellow)],
+    text: '#tbd'
+    // light: [null, tint(0.9, baseColors.red)],
+    // dark: [null, shade(0.4, baseColors.red)]
   },
   red: {
     base: baseColors.red,
-    text: readableColor(baseColors.red),
-    light: [null, tint(0.9, baseColors.red)],
-    dark: [null, shade(0.4, baseColors.red)],
-  },
+    text: '#520000'
+    // light: [null, tint(0.9, baseColors.red)],
+    // dark: [null, shade(0.4, baseColors.red)]
+  }
 };
 
-const blurple = colors.blurple;
+const purple = colors.purple;
 const blue = colors.blue;
 const green = colors.green;
-const yellow = colors.yellow;
+// const yellow = colors.yellow;
 const red = colors.red;
 
-export { blurple };
+export { purple };
 export { blue };
+// export { yellow };
 export { green };
-export { yellow };
 export { red };
 
 export default baseColors;
