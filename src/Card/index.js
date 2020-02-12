bd995b095071e4395955021fbafc0f5b8d7579c8
Alex Singh <alex@alexsingh.com>
2020-02-12 16:15:32 -0300

    adds overflow:hidden and an explicit 300px width

--- src/Card/index.js
+++ src/Card/index.js
@@ -5,11 +5,13 @@ import Box from '../Box'
 const Card = forwardRef(({ ...props }, ref) => (
   <Box
     display="inline-block"
+    width={300}
     p={3}
     border={1}
     borderRadius={2}
     borderWidth={1}
     color="text"
+    overflow="hidden"
     ref={ref}
     {...props}
   ></Box>
