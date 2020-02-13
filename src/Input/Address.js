38403e2c455663a4b3768c27247be26213a1a70f
Alex Singh <alex@alexsingh.com>
2020-02-13 17:26:08 -0300

    Updating Address Input. Init.

--- src/Input/Address.js
+++ src/Input/Address.js
@@ -1,16 +1,26 @@
 import React, { forwardRef } from 'react'
 import { func, string } from 'prop-types'
+import { Box, Input } from '../'
 
 const Address = forwardRef(
   ({ onChange, value, placeholder, label, error, ...props }, ref) => {
     // TODO: address validation occurs here @jon
     return (
-      <div ref={ref} {...props}>
-        <div>{label}</div>
+      <Box
+        display="inline-block"
+        alignContent="center"
+        border={1}
+        borderRadius={1}
+        ref={ref}
+        {...props}
+      >
+        <Box display="inline-block" mx={3}>
+          {label}
+        </Box>
         {/* how do we chunk the address in the input component? */}
         <input onChange={onChange} value={value} placeholder="t1..." />
         {error && <p>{error}</p>}
-      </div>
+      </Box>
     )
   }
 )
