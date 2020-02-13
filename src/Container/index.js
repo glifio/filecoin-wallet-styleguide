0473947e6e0f195fc407c8c15dfe51f051dd0d48
Alex Singh <alex@alexsingh.com>
2020-02-13 11:33:17 -0300

    Updates Container styles to only use  responsive padding

--- src/Container/index.js
+++ src/Container/index.js
@@ -3,5 +3,5 @@ import Box from '../Box'
 
 export default forwardRef(({ ...props }, ref) => (
   // @ALEX - some sweet responsiveness perks with s-s https://styled-system.com/responsive-styles
-  <Box ref={ref} {...props} mt="20px" ml="20vw" mr="20vw" />
+  <Box ref={ref} {...props} p={[1, 3, 4]} />
 ))
