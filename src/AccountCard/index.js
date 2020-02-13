18326e24fb2eae1a80d5e340801acf832e9ea0f6
Alex Singh <alex@alexsingh.com>
2020-02-13 16:32:02 -0300

    References text styles from new Typography file

--- src/AccountCard/index.js
+++ src/AccountCard/index.js
@@ -1,7 +1,8 @@
 import React, { forwardRef } from 'react'
 import { string, func } from 'prop-types'
 import { ADDRESS_PROPTYPE } from '../customPropTypes'
-import { Box, Glyph, Button, Text } from '../'
+import { Box, Glyph, Button } from '../'
+import { BigTitle, Text } from '../Typography'
 
 /*
  @ALEX - should we consider extending the ColoredContainer component for this?
@@ -36,7 +37,7 @@ const AccountCard = forwardRef(
         <Text>Account</Text>
       </Box>
       <Box color="lightpurple">
-        <Text>{alias}</Text>
+        <BigTitle textStyle="BigTitle">{alias}</BigTitle>
         <Text>{truncate(address)}</Text>
       </Box>
       <Box display="flex">
