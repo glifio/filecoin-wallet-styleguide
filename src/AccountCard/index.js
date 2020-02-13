e2d0d1d0df2b5a0963c99e17c0beb7d96a8b4fc9
Alex Singh <alex@alexsingh.com>
2020-02-13 13:09:16 -0300

    Updates AccountCard styling, structure (WIP)

--- src/AccountCard/index.js
+++ src/AccountCard/index.js
@@ -1,7 +1,7 @@
 import React, { forwardRef } from 'react'
 import { string, func } from 'prop-types'
 import { ADDRESS_PROPTYPE } from '../customPropTypes'
-import { Box } from '../'
+import { Box, Glyph, Button, Text } from '../'
 
 /*
  @ALEX - should we consider extending the ColoredContainer component for this?
@@ -10,123 +10,54 @@ import { Box } from '../'
 const truncate = string => `${string.slice(0, 4)}...${string.slice(-4)}`
 
 const AccountCard = forwardRef(
-  ({ address, alias, color, onAccountSwitch, ...props }, ref) => (
+  ({ address, alias, onAccountSwitch, ...props }, ref) => (
     <Box
       display="flex"
       flexDirection="column"
+      justifyContent="space-between"
       width={11}
       height={11}
       borderRadius={2}
-      bg="primary"
+      p={3}
+      color="lightpurple"
+      bg="purple"
       boxShadow={1}
       ref={ref}
       {...props}
     >
-      <div
-        css={`
-          margin-left: 12px;
-          display: flex;
-          flex-direction: column;
-          height: 100%;
-          justify-content: space-between;
-        `}
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        color="lightpurple"
+        maxWidth={160}
       >
-        <div
-          css={`
-            margin-top: 12px;
-            display: flex;
-            flex-direction: row;
-            align-items: center;
-          `}
-        >
-          <div
-            css={`
-              width: 50px;
-              height: 48px;
-              border: 3px solid #d9d0fe;
-              display: flex;
-              flex-direction: column;
-              justify-content: center;
-            `}
-          >
-            <p
-              css={`
-                font-weight: bold;
-                color: #d9d0fe;
-                font-size: 24px;
-                align-self: center;
-                font-family: RT Alias Fine Test;
-              `}
-            >
-              Ac
-            </p>
-          </div>
-          <p
-            css={`
-              margin-left: 12px;
-              font-weight: bold;
-              color: #d9d0fe;
-              font-size: 24px;
-              font-family: RT Alias Fine Test;
-            `}
-          >
-            Account
-          </p>
-        </div>
-        <div
-          css={`
-            margin-bottom: 24px;
-          `}
-        >
-          <div>
-            <p
-              css={`
-                font-weight: bold;
-                color: #eeebfe;
-                font-size: 64px;
-                font-family: RT Alias Fine Test;
-                font-weight: bold;
-                margin: 0;
-                height: 60px;
-              `}
-            >
-              {alias}
-            </p>
-          </div>
-          <div>
-            <p
-              css={`
-                font-weight: bold;
-                color: #e7e3fe;
-                font-size: 24px;
-                font-family: RT Alias Fine Test;
-                font-weight: normal;
-                margin: 0;
-              `}
-            >
-              {truncate(address)}
-            </p>
-          </div>
-        </div>
-        <button
+        <Glyph color="lightpurple" acronym="Ac"></Glyph>
+        <Text>Account</Text>
+      </Box>
+      <Box color="lightpurple">
+        <Text>{alias}</Text>
+        <Text>{truncate(address)}</Text>
+      </Box>
+      <Box display="flex">
+        <Button
+          type="secondary"
+          title="Switch"
           onClick={onAccountSwitch}
-          css={`
-            margin-bottom: 12px;
-            width: 90px;
-            height: 36px;
-            border: 1px solid #e9eeff;
-            outline: none;
-            background-color: Transparent;
-            color: #d9d0fe;
-            font-size: 18px;
-            font-weight: bold;
-            font-family: RT Alias Fine Test;
-            cursor: pointer;
-          `}
-        >
-          Switch
-        </button>
-      </div>
+          color="lightpurple"
+          borderColor="lightpurple"
+          px={2}
+        />
+        <Button
+          type="secondary"
+          title="View on Ledger"
+          onClick={onAccountSwitch}
+          color="lightpurple"
+          borderColor="lightpurple"
+          ml={2}
+          px={2}
+        />
+      </Box>
     </Box>
   )
 )
