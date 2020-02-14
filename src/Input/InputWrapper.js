4770b502926ac9379235abaaa2c8addf08c9c833
Alex Singh <alex@alexsingh.com>
2020-02-14 11:50:58 -0300

    Adds InputWrapper component

---
+++ src/Input/InputWrapper.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+import { space, color, layout, border, flexbox } from 'styled-system'
+
+//input wrapper
+export default styled.div`
+  ${color}
+  ${space}
+  ${layout}
+  ${border}
+  ${flexbox}
+`
