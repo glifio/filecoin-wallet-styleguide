0e626e4a609408643b0b17620b5d4a980f7ba565
Alex Singh <alex@alexsingh.com>
2020-02-14 11:51:11 -0300

    Updating Address Input styling (WIP)

--- src/Input/Address.js
+++ src/Input/Address.js
@@ -1,16 +1,46 @@
 import React, { forwardRef } from 'react'
 import { func, string } from 'prop-types'
+import InputWrapper from './InputWrapper'
+import { Box, Input } from '../'
 
 const Address = forwardRef(
   ({ onChange, value, placeholder, label, error, ...props }, ref) => {
     // TODO: address validation occurs here @jon
     return (
-      <div ref={ref} {...props}>
-        <div>{label}</div>
-        {/* how do we chunk the address in the input component? */}
-        <input onChange={onChange} value={value} placeholder="t1..." />
-        {error && <p>{error}</p>}
-      </div>
+      <InputWrapper display="inline-block" border={1} borderRadius={1}>
+        <Box ref={ref} {...props}>
+          <Box display="inline" px={3}>
+            {label}
+          </Box>
+          {/* how do we chunk the address in the input component? */}
+          <input
+            css={`
+              display: inline-block;
+              padding: 0.875rem 0.5rem;
+              border: 0;
+              border-radius: 4px;
+              border-left: 1px solid;
+              border-top-left-radius: 0px;
+              border-bottom-left-radius: 0px;
+              transition: 0.2s ease-in-out;
+
+              &:hover {
+                background: #f5f5f5;
+              }
+
+              &:focus {
+                box-shadow: 0;
+                outline: 0;
+                background: #f5f5f5;
+              }
+            `}
+            onChange={onChange}
+            value={value}
+            placeholder="t1..."
+          />
+          {error && <p>{error}</p>}
+        </Box>
+      </InputWrapper>
     )
   }
 )
