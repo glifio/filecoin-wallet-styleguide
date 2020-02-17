c366450ff322d9392489ace38c3bd8d066a2a013
Alex Singh <alex@alexsingh.com>
2020-02-17 19:27:17 -0300

    Init. Matching Funds input object to Figma design

--- src/Input/Funds.js
+++ src/Input/Funds.js
@@ -1,16 +1,60 @@
 import React, { useState, forwardRef } from 'react'
 import { func, string, shape } from 'prop-types'
+import { Box } from '../'
+import { Text } from '../Typography'
 
 const Funds = forwardRef(
-  ({ onChange, value, placeholder, label, error }, ref) => {
+  ({ onChange, value, placeholder, error, ...props }, ref) => {
     // TODO: conversion between rates
     return (
-      <div ref={ref} {...props}>
-        <div>Amount</div>
+      <Box
+        display="flex"
+        maxWidth="480px"
+        minHeight="160px"
+        border={1}
+        ref={ref}
+        {...props}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          flexGrow="1"
+          width="280px"
+          textAlign="center"
+          borderRight="1px solid #444"
+        >
+          <Text>Amount</Text>
+        </Box>
         {/* how do we chunk the address in the input component? */}
-        <input onChange={onChange} value={value} placeholder="t1..." />
+        <Box display="inline-block" width="280px">
+          <Box display="block" height="80px" width="100%">
+            <input
+              css={`
+                height: 100%;
+                width: 100%;
+                border: 0;
+              `}
+              onChange={onChange}
+              value={value}
+              placeholder={placeholder}
+            />
+          </Box>
+          <Box display="block" height="80px" width="100%" borderTop="1px solid">
+            <input
+              css={`
+                height: 100%;
+                width: 100%;
+                border: 0;
+              `}
+              onChange={onChange}
+              value={value}
+              placeholder={placeholder}
+            />
+          </Box>
+        </Box>
         {error && <p>{error}</p>}
-      </div>
+      </Box>
     )
   }
 )
@@ -19,7 +63,8 @@ Funds.propTypes = {
   onChange: func.isRequired,
   label: string.isRequired,
   value: string,
-  error: string
+  error: string,
+  placeholder: string
 }
 
 Funds.defaultProps = {
