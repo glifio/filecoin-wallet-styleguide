03db3f371117aba3a50ef69bf6664a0b94438a4d
Alex Singh <alex@alexsingh.com>
2020-02-17 19:42:00 -0300

    Updates BaseInput styling

--- src/Input/BaseInput.js
+++ src/Input/BaseInput.js
@@ -4,12 +4,14 @@ import { space, color, layout, border, flexbox } from 'styled-system'
 //input wrapper
 export default styled.input`
     display: inline-block;
+    padding-left: ${props => props.theme.space[2]}px;
+    padding-right: ${props => props.theme.space[2]}px;
     border: 0;
-    border-radius: 4px;
-    border-left: 1px solid;
+    border-radius: ${1}
     border-top-left-radius: 0px;
     border-bottom-left-radius: 0px;
     transition: 0.2s ease-in-out;
+    text-align: center;
 
     &:hover {
     background: #f5f5f5;
