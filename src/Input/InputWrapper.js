58e81196925199106b827f24b8528533c3003d89
Alex Singh <alex@alexsingh.com>
2020-02-18 08:57:22 -0300

    Adds styles

--- src/Input/InputWrapper.js
+++ src/Input/InputWrapper.js
@@ -3,6 +3,9 @@ import { space, color, layout, border, flexbox } from 'styled-system'
 
 //input wrapper
 export default styled.div`
+  display: inline-block;
+  border: ${props => props.theme.borders[1]};
+  border-radius: ${props => props.theme.radii[1]};
   ${color}
   ${space}
   ${layout}
