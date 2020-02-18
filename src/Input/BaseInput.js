119ff94b6751d43ca0f1952c82a98e41f9ac5a9a
Alex Singh <alex@alexsingh.com>
2020-02-18 08:50:16 -0300

    Updates styling, pulls some values from theme file

--- src/Input/BaseInput.js
+++ src/Input/BaseInput.js
@@ -6,12 +6,12 @@ export default styled.input`
     display: inline-block;
     padding-left: ${props => props.theme.space[2]}px;
     padding-right: ${props => props.theme.space[2]}px;
-    border: 0;
-    border-radius: ${1}
-    border-top-left-radius: 0px;
-    border-bottom-left-radius: 0px;
+    border: ${props => props.theme.borders[1]}
+    border-radius: ${props => props.theme.radii[1]}
+    /* border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px; */
     transition: 0.2s ease-in-out;
-    text-align: center;
+    text-align: right;
 
     &:hover {
     background: #f5f5f5;
