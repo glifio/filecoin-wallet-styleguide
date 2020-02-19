9a13db91421aa1fb4ebfeddea7f451f046869dc0
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-19 17:51:09 -0500

    Add address validation to inputs

--- src/Input/Address.js
+++ src/Input/Address.js
@@ -1,18 +1,27 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 import { func, string } from 'prop-types'
+import { validateAddressString } from '@openworklabs/filecoin-address'
 import TextInput from './Text'
 
 const Address = forwardRef(
-  ({ onChange, value, placeholder, label, error, ...props }, ref) => {
+  ({ onChange, value, placeholder, label, ...props }, ref) => {
+    const [addressValidationError, setAddressValidationError] = useState(false)
     return (
       <TextInput
+        onBlur={() => {
+          const isValidAddress = validateAddressString(value)
+          if (value && !isValidAddress) setAddressValidationError(true)
+        }}
+        onFocus={() => {
+          if (addressValidationError) setAddressValidationError(false)
+        }}
         ref={ref}
         {...props}
         label={label}
         onChange={onChange}
         value={value}
         placeholder={placeholder}
-        error={error}
+        error={addressValidationError && `Invalid ${label} address.`}
       />
     )
   }
