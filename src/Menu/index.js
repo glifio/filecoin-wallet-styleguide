02dd9d8cfc48ef2edc4c9342773914e6df7e6e91
Alex Singh <alex@alexsingh.com>
2020-02-19 07:50:25 -0300

    Moves Menu & MenuItem into their own component folder

---
+++ src/Menu/index.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+export const Menu = styled.ul`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  ${space}
+  ${color}
+  ${layout}
+  ${border}
+  ${flexbox}
+`
+
+export const MenuItem = styled.li`
+  ${space}
+  ${color}
+  ${layout}
+  ${flexbox}
+`
--- src/index.js
+++ src/index.js
@@ -21,24 +21,6 @@ export { default as Box } from './Box'
 // typography
 export * from './Typography'
 
-export const Menu = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  ${space}
-  ${color}
-  ${layout}
-  ${border}
-  ${flexbox}
-`
-
-export const MenuItem = styled.li`
-  ${space}
-  ${color}
-  ${layout}
-  ${flexbox}
-`
-
 export const Icon = styled.span`
   display: inline-block;
   ${space}
