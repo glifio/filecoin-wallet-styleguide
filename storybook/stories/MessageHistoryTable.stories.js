f0a9155c77ada8363f37eb30b7781e69113308d6
Alex Singh <alex@alexsingh.com>
2020-02-19 12:55:35 -0300

    Adds MessageHistory story

---
+++ storybook/stories/MessageHistoryTable.stories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {
+  MessageHistoryTable,
+  MessageHistoryRow
+} from '@openworklabs/filecoin-wallet-styleguide'
+
+export default {
+  title: 'MessageHistoryTable'
+}
+
+export const MessageHistoryTablePrimary = () => <MessageHistoryTable />
+
+export const MessageHistoryRowPrimary = () => <MessageHistoryRow />
