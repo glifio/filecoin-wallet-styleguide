231b2585528396512b47cf54dcd538652b661189
Alex Singh <alex@alexsingh.com>
2020-02-20 12:24:32 -0300

    Initial styling, structure complete

--- src/MessageHistoryTable/MessageHistoryRow.js
+++ src/MessageHistoryTable/MessageHistoryRow.js
@@ -3,6 +3,7 @@ import { MESSAGE_PROPS } from '../customPropTypes'
 import Box from '../Box'
 import { Menu, MenuItem } from '../Menu'
 import { Text, Label } from '../Typography'
+import { IconSend, IconReceive } from '../Icons'
 
 const MessageHistoryRow = ({
   to,
@@ -19,16 +20,25 @@ const MessageHistoryRow = ({
         <MenuItem display="flex" flexDirection="row">
           <Menu display="flex" flexDirection="column">
             <MenuItem>
-              <Text m={0}>{status}</Text>
-            </MenuItem>
-            <MenuItem>
-              <Label my={2}>Date</Label>
+              {/* @jon - Todo - Map {status} to Icon prop color
+              <Text m={0}>{status}</Text> */}
+              <IconSend />
             </MenuItem>
           </Menu>
-          <Menu display="flex" flex-wrap="wrap" mx={3}>
+          <Menu display="flex" flexDirection="column" ml={3}>
+            <MenuItem>
+              <Text my={0}>Sending</Text>
+            </MenuItem>
+            <MenuItem>
+              <Text mt={2} mb={0}>
+                Date
+              </Text>
+            </MenuItem>
+          </Menu>
+          <Menu display="flex" flex-wrap="wrap" ml={3}>
             <MenuItem overflow="hidden" maxWidth={120}>
               <Text my={0}>To</Text>
-              <Text my={2}>
+              <Text mt={2} mb={0}>
                 {to}
                 {from}
               </Text>
@@ -39,15 +49,40 @@ const MessageHistoryRow = ({
           </Menu>
         </MenuItem>
       </Menu>
-      <Menu display="flex" flexDirection="column" flex-wrap="wrap">
-        <MenuItem display="flex">
-          <Text my={0}>{value}</Text>
-          <Text my={0} ml={2}>
-            FIL
-          </Text>
+      <Menu display="flex" flexDirection="row">
+        <MenuItem>
+          <Menu
+            display="flex"
+            flexDirection="column"
+            alignItems="flex-end"
+            flex-wrap="wrap"
+            ml={3}
+          >
+            <MenuItem display="flex">
+              <Text my={0}>{value}</Text>
+            </MenuItem>
+            <MenuItem display="flex">
+              <Text mt={2} mb={0}>
+                500
+              </Text>
+            </MenuItem>
+          </Menu>
         </MenuItem>
         <MenuItem>
-          <Text my={2}>fiat</Text>
+          <Menu
+            display="flex"
+            flexDirection="column"
+            alignItems="flex-start"
+            flex-wrap="wrap"
+            ml={3}
+          >
+            <MenuItem>
+              <Text my={0}>FIL</Text>
+              <Text mt={2} mb={0}>
+                Fiat
+              </Text>
+            </MenuItem>
+          </Menu>
         </MenuItem>
       </Menu>
     </Box>
