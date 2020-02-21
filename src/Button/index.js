a22dadf30bd7946ad72e53bccfabe810bb82852e
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-02-21 12:04:40 -0500

    Update button styles

--- src/Button/index.js
+++ src/Button/index.js
@@ -2,32 +2,14 @@ import React, { forwardRef } from 'react'
 import { func, bool, string, oneOf } from 'prop-types'
 import BaseButton from './BaseButton'
 
-const colorMatcher = {
-  color: {
-    primary: 'core.nearblack',
-    secondary: 'core.nearblack'
-  },
-  backgroundColor: {
-    primary: 'background.button.primary',
-    secondary: 'background.button.secondary'
-  },
-  borderColor: {
-    primary: 'background.button.primary',
-    secondary: 'core.darkgray'
-  }
-}
-
 const Button = forwardRef(
   ({ buttonStyle, disabled, onClick, title, ...props }, ref) => (
     <BaseButton
+      buttonStyle={buttonStyle}
       py={3}
       px={4}
       fontSize={3}
-      border={1}
       borderRadius={2}
-      backgroundColor={colorMatcher.backgroundColor[buttonStyle]}
-      color={colorMatcher.color[buttonStyle]}
-      borderColor={colorMatcher.borderColor[buttonStyle]}
       onClick={onClick}
       disabled={disabled}
       ref={ref}
